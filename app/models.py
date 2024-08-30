dd4657743157f15119affbac9e2f94077fec0c20
Kevin Miller <kevinvanissa@gmail.com>
2024-08-29 20:57:08 -0400

    Update size of password field

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     username = db.Column(db.String(120), nullable=False)
-    password = db.Column(db.String(140))
+    password = db.Column(db.String(256))
 
     def get_id(self):
         return str(self.id)
